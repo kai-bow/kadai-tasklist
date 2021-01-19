1f831d125d55c3c00b3df29fc2548b9a264f1c5b
kai-bow <onishikaisei0612@eis.hokudai.ac.jp>
2021-01-19 17:41:20 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -50,6 +50,12 @@ class TasksController extends Controller
      // postでtasks/にアクセスされた場合の「新規登録処理
     public function store(Request $request)
     {
+        
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:10',
+        ]);
+        
         // タスクを作成
         $task = new Task;
         $task->content = $request->content;
@@ -105,6 +111,11 @@ class TasksController extends Controller
      // putまたはpatchでtasks/idにアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:10',
+        ]);
+        
          // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
